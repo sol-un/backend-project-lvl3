13acbb4be08ff185e8d891c29f5613cad1fb64f6
Andrey Solovyev <solovyev833@gmail.com>
2020-08-21 12:34:48 +0300

    Step 2 complete

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -3,8 +3,10 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import os from 'os';
 import nock from 'nock';
+import cheerio from 'cheerio';
 import pageLoader from '../index.js';
 
+const getFixturePath = (filepath) => path.join(path.resolve(), '__fixtures__', filepath);
 let tempDir;
 
 beforeEach(async () => {
@@ -13,11 +15,21 @@ beforeEach(async () => {
 
 test('Page loader', async () => {
   nock(/test\.com/)
-    .get('/testfile')
-    .reply(200, 'test page content');
+    .get('/test_page')
+    .replyWithFile(200, getFixturePath(path.join('before', 'test_page.html')))
+    .get('/styles.css')
+    .replyWithFile(200, getFixturePath(path.join('before', 'styles.css')))
+    .get('/assets/placeholder_image.png')
+    .replyWithFile(200, getFixturePath(path.join('before', 'assets', 'placeholder_image.png')))
+    .get('/assets/script.js')
+    .replyWithFile(200, getFixturePath(path.join('before', 'assets', 'script.js')));
 
-  await pageLoader('https://test.com/testfile', tempDir);
-  const contents = await fs.readFile(path.join(tempDir, 'test-com-testfile.html'), 'utf8');
+  await pageLoader('https://test.com/test_page', tempDir);
+  const actualPage = await fs.readFile(path.join(tempDir, 'test-com-test-page.html'), 'utf8');
+  const expectedPage = await fs.readFile(getFixturePath(path.join('after', 'test-com-test-page.html')), 'utf8');
+  const actualAssets = await fs.readdir(path.join(tempDir, 'test-com-test-page_files'));
+  const expectedAssets = await fs.readdir(getFixturePath(path.join('after', 'test-com-test-page_files')));
 
-  expect(contents).toEqual('test page content');
+  expect(cheerio.load(actualPage).html()).toBe(cheerio.load(expectedPage).html());
+  expect(actualAssets).toStrictEqual(expectedAssets);
 });
--- src/page-loader.js
+++ src/page-loader.js
@@ -2,12 +2,46 @@ import { promises as fs } from 'fs';
 import process from 'process';
 import axios from 'axios';
 import path from 'path';
+import cheerio from 'cheerio';
+
+const dispatcherByTagName = {
+  script: () => 'src',
+  img: () => 'src',
+  link: () => 'href',
+};
 
 export default (link, dest = process.cwd()) => {
   const url = new URL(link);
-  const filename = `${url.hostname}${url.pathname}`.replace(/[^\d\w]/g, '-').concat('.html');
+  const filename = `${url.hostname}${url.pathname}`.replace(/([^\d\w]|_)/g, '-').concat('.html');
+  const dirname = `${url.hostname}${url.pathname}`.replace(/([^\d\w]|_)/g, '-').concat('_files');
+  const assetsData = [];
   return axios.get(link)
     .then((response) => {
-      fs.writeFile(path.join(dest, filename), response.data, 'utf8');
+      const $ = cheerio.load(response.data);
+      $('img, script, link')
+        .each((_i, tag) => {
+          const attrName = dispatcherByTagName[tag.tagName](tag);
+          const attrValue = $(tag).attr(attrName);
+          if (attrValue.includes(url.hostname) || !attrValue.includes('http')) {
+            const assetObj = {
+              link: new URL(attrValue, url.origin).toString(),
+              path: path.join(dirname, attrValue.replace(/([^\d\w.]|_)/g, '-')),
+            };
+            assetsData.push(assetObj);
+            $(tag).attr(`${attrName}`, assetObj.path);
+          }
+        });
+      fs.writeFile(path.join(dest, filename), $.html());
+    })
+    .then(() => {
+      fs.mkdir(path.join(dest, dirname));
+      const promiseArr = assetsData.map((item) => axios({
+        url: item.link,
+        method: 'get',
+        responseType: 'arraybuffer',
+      }).then((response) => {
+        fs.writeFile(path.join(dest, item.path), response.data);
+      }));
+      return Promise.all(promiseArr);
     });
 };
